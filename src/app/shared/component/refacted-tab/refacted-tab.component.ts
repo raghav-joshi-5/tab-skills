16d77ffa898348ab4eb28366c4f439dce272943c
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-12-29 14:00:56 +0530

    update array

--- src/app/shared/component/refacted-tab/refacted-tab.component.ts
+++ src/app/shared/component/refacted-tab/refacted-tab.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Itab } from '../../module/tab';
+import { tabs } from '../../const/tab';
 
 @Component({
   selector: 'app-refacted-tab',
@@ -13,38 +14,5 @@ export class RefactedTabComponent implements OnInit {
 
   selectedSkill: string = 'html';
 
-  tabArr: Array<Itab> = [
-    {
-      tabtitle: 'HTML',
-      tabContent: `<p class="alert alert-warning"><strong>HTML</strong> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Laudantium dolor unde architecto, animi tenetur
-            molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
-            beatae quidem!</p>`,
-      skillName: 'html',
-    },
-    {
-      tabtitle: 'CSS',
-      tabContent: `<p class="alert alert-warning"><strong>css</strong> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Laudantium dolor unde architecto, animi tenetur
-            molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
-            beatae quidem!</p>`,
-      skillName: 'css',
-    },
-    {
-      tabtitle: 'JAVASCRIPT',
-      tabContent: `<p class="alert alert-warning"><strong>javascript</strong> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Laudantium dolor unde architecto, animi tenetur
-            molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
-            beatae quidem!</p>`,
-      skillName: 'javascript',
-    },
-    {
-      tabtitle: 'ANGULAR 14',
-      tabContent: `<p class="alert alert-warning"><strong>angular 14</strong> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Laudantium dolor unde architecto, animi tenetur
-            molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
-            beatae quidem!</p>`,
-      skillName: 'angular 14',
-    },
-  ];
+  tabArr: Array<Itab> = tabs;
 }
---
+++ src/app/shared/const/tab.ts
@@ -0,0 +1,36 @@
+import { Itab } from '../module/tab';
+
+export const tabs: Array<Itab> = [
+  {
+    tabtitle: 'HTML',
+    tabContent: `<p class="alert alert-warning"><strong>HTML</strong> Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Laudantium dolor unde architecto, animi tenetur
+              molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
+              beatae quidem!</p>`,
+    skillName: 'html',
+  },
+  {
+    tabtitle: 'CSS',
+    tabContent: `<p class="alert alert-warning"><strong>css</strong> Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Laudantium dolor unde architecto, animi tenetur
+              molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
+              beatae quidem!</p>`,
+    skillName: 'css',
+  },
+  {
+    tabtitle: 'JAVASCRIPT',
+    tabContent: `<p class="alert alert-warning"><strong>javascript</strong> Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Laudantium dolor unde architecto, animi tenetur
+              molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
+              beatae quidem!</p>`,
+    skillName: 'javascript',
+  },
+  {
+    tabtitle: 'ANGULAR 14',
+    tabContent: `<p class="alert alert-warning"><strong>angular 14</strong> Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Laudantium dolor unde architecto, animi tenetur
+              molestias explicabo iste iure omnis ratione quibusdam numquam, culpa
+              beatae quidem!</p>`,
+    skillName: 'angular 14',
+  },
+];
